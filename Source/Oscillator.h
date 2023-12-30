00431bae7ec7f4aad8e69e2c796b2144df171a56
eryk-urbanski <eryk.urbanski14@gmail.com>
2023-12-30 19:58:14 +0100

    Added a faster sine algorithm - direct-form digital resonator

--- Source/Oscillator.h
+++ Source/Oscillator.h
@@ -20,15 +20,21 @@ public:
     void reset()
     {
         phase = 0;
+        sin0 = amplitude * std::sin(phase * TWO_PI);
+        sin1 = amplitude * std::sin((phase - inc) * TWO_PI);
+        dsin = 2.0f * std::cos(inc * TWO_PI);
     }
 
     float nextSample()
     {
-        phase += inc;
-        if (phase >= 1.0f) {
-            phase -= 1.0f;
-        }
-
-        return amplitude * std::sin(TWO_PI * phase);
+        float sinx = dsin * sin0 - sin1;
+        sin1 = sin0;
+        sin0 = sinx;
+        return sinx;
     }
+
+private:
+    float sin0;
+    float sin1;
+    float dsin;
 };
