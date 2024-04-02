f6d4c07b438df2a6ccd96a80238f384e5d382726
eryk-urbanski <eryk.urbanski14@gmail.com>
2024-04-02 12:34:40 +0200

    Added pitch bendiing

--- Source/Synth.cpp
+++ Source/Synth.cpp
@@ -28,6 +28,7 @@ void Synth::reset()
 {
     voice.reset();
     noiseGen.reset();
+    pitchBend = 1.0f;
 }
 
 void Synth::render(float** outputBuffers, int sampleCount)
@@ -35,7 +36,7 @@ void Synth::render(float** outputBuffers, int sampleCount)
     float* outputBufferLeft = outputBuffers[0];
     float* outputBufferRight = outputBuffers[1];
     
-    voice.osc1.period = voice.period;
+    voice.osc1.period = voice.period * pitchBend;
     voice.osc2.period = voice.osc1.period * detune;
 
     for (int sample = 0; sample < sampleCount; ++sample) {
@@ -63,6 +64,11 @@ void Synth::render(float** outputBuffers, int sampleCount)
 void Synth::midiMessage(uint8_t data0, uint8_t data1, uint8_t data2)
 {
     switch (data0 & 0xF0) {
+        // Pitch bend
+        case 0xE0:
+            pitchBend = std::exp(-0.000014102f * float(data1 + 128 * data2 - 8192));
+            break;
+
         // Note off
         case 0x80:
             noteOff(data1 & 0x7F);
--- Source/Synth.h
+++ Source/Synth.h
@@ -38,4 +38,6 @@ private:
     float sampleRate;
     Voice voice;
     NoiseGenerator noiseGen;
+
+    float pitchBend;
 };
